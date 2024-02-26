a01a8242995fa2c0b519f58924e7e2b96c43ec66
moududulhaque <moududulhaque@gmail.com>
2024-02-26 12:13:44 +0600

    Premitive and Non-Premitive

---
+++ js-analysis/es6-conceptual.js
@@ -0,0 +1,15 @@
+//Premitive
+
+//non-premitive
+const b = { name: "Moudud" };
+const nums = [1, 2, 3];
+
+let num1 = nums;
+num1.push(3);
+console.log(num1);
+console.log(nums);
+
+let c = b;
+c.age = 27;
+console.log(b);
+console.log(c);
