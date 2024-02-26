045f25f230cfe43f8a108ea0a4ba0ae6bf6b457f
moududulhaque <moududulhaque@gmail.com>
2024-02-26 12:42:42 +0600

    Truthy and Falsy Value

--- js-analysis/es6-conceptual.js
+++ js-analysis/es6-conceptual.js
@@ -1,6 +1,7 @@
 //Premitive
 
 //non-premitive
+//reference type
 const b = { name: "Moudud" };
 const nums = [1, 2, 3];
 
@@ -11,5 +12,19 @@ console.log(nums);
 
 let c = b;
 c.age = 27;
+
+let f = c;
+f.country = "Bangladesh";
 console.log(b);
 console.log(c);
+console.log(f);
+
+//Truthy and Falsy Value
+
+//False Value => false,0,"",Null,undefined,Nan
+//truthy=> true,"Moudud",12345,1,[],{}
+//How can I check truthy or falsy\
+const value = "Moudud";
+const value1 = "";
+console.log(!!value);//true
+console.log(!!value1);//falsy
